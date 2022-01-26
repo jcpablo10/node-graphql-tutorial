9fc2346ae844ebbe142a748d23ad3eee2e39062d
Juan C Pablo <jcpablo10@gmail.com>
2022-01-26 12:11:41 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const app = express()
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+}));
 
 app.listen(4000, () => {
-  console.log('Listening app on port 4000');
+  console.log('Listening app on port http://localhost:4000');
 })
